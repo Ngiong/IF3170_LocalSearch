708177627c8d44bdc781f0f93a953edae832f57a
Micky Yu <mickyyu96@gmail.com>
2016-10-03 23:10:34 +0700

    rapiin kode

--- Src/Problem.hpp
+++ Src/Problem.hpp
@@ -20,14 +20,10 @@ class Problem {
 		void solveUsingHill(int maxRestart);
 		void solveUsingSA(double, double, int, int);
 		void solveUsingGA(int, int);
-		void solveUsingKocokan(int); // maxSteps
 		Problem modifySolution(Problem P);
 
 		// Init
 		void initByRandom();
-		void initByMikir(); /* ================== BERHARAP BANYAK DENGAN METHOD INI ==========================*/
-		// Salah satu yg bisa dipertimbangkan untuk initByMikir adalah menjadikan sebuah matrix terlebih dahulu,
-		// lalu melakukan assignment u/ slot-slot yang hanya mungkin di occupy oleh 1 matakuliah
 
 		// Performance Measure
 		bool isSolved();
@@ -57,25 +53,8 @@ class Problem {
 		RandomGenerator rnd;
 };
 
-Problem::Problem() {
-	nRooms = 4; nCourses = 8;
-	room = new Ruang * [4];
-	room[0] = new Ruang("7602", 7, 14, 1,1,1,1,1); // perhatikan kalau kita maunya 1-based index u/ array
-	room[1] = new Ruang("7603", 7, 14, 1,0,1,0,1);
-	room[2] = new Ruang("7610", 9, 12, 1,1,1,1,1);
-	room[3] = new Ruang("Labdas2", 10, 14, 0,1,0,1,0);
-	course = new Kuliah * [8];
-	course[0] = new Kuliah("IF2110", "7602", 7, 12, 4, 1,1,1,1,1);
-	course[1] = new Kuliah("IF2130", "-",   10, 16, 3, 0,0,1,1,0);
-	course[2] = new Kuliah("IF2150", "-",    9, 13, 2, 1,0,1,0,1);
-	course[3] = new Kuliah("IF2170", "7610", 7, 12, 3, 1,1,1,1,1);
-	course[4] = new Kuliah("IF3110", "7602", 7,  9, 2, 1,1,1,1,1);
-	course[5] = new Kuliah("IF3130", "-",    7, 12, 2, 0,0,1,1,1);
-	course[6] = new Kuliah("IF3170", "7602", 7,  9, 2, 1,1,1,1,1);
-	course[7] = new Kuliah("IF3111", "-",    7, 12, 2, 1,1,1,1,1);
-}
+Problem::Problem() {}
 
-// Ntar dikasih fitur untuk nge-parse juga dari file
 Problem::Problem(string file) {
 	string line;
 	nRooms = nCourses = 0;
@@ -94,7 +73,6 @@ Problem::Problem(string file) {
 		}
 		myfile.close();
 
-		// create
 		room = new Ruang * [nRooms];
 		course = new Kuliah * [nCourses];
 	}
@@ -150,7 +128,6 @@ Problem::Problem(string file) {
 				}
  				j++;
 			}
-			cout << ruangan << " " << startTime << " " << finishTime << " " << isSenin << " " << isSelasa << " " << isRabu << " " << isKamis << " " << isJumat << endl;
 			room[i] = new Ruang(ruangan, startTime, finishTime, isSenin, isSelasa, isRabu, isKamis, isJumat);
 		}
 
@@ -206,8 +183,6 @@ Problem::Problem(string file) {
 				}
  				j++;
 			}
-
-			cout << jadwal << " " << ruangan << " " << startTime << " " << finishTime << " " << durasi << " " << isSenin << " " << isSelasa << " " << isRabu << " " << isKamis << " " << isJumat << endl;
 			course[i] = new Kuliah(jadwal, ruangan, startTime, finishTime, durasi, isSenin, isSelasa, isRabu, isKamis, isJumat);
 		}
 		myfile.close();
@@ -215,10 +190,6 @@ Problem::Problem(string file) {
 	else {
 		cout << "Unable to open file." << endl;
 	}
-	/******************************************
-	*    Edit2 Spek Ruang dan Kuliah di sini
-	*******************************************/
-	// cout << rooms[1].ruangName << endl; // Bisa karena sudah di FRIEND kan biar gampang gitchuuu :v
 }
 
 Problem::Problem(const Problem &P) {
@@ -236,14 +207,11 @@ Problem::Problem(const Problem &P) {
 		course[i] = new Kuliah(P.course[i]->kode, P.course[i]->butuhRuang, P.course[i]->start, P.course[i]->end, P.course[i]->duration,
 					P.course[i]->days[1], P.course[i]->days[2], P.course[i]->days[3], P.course[i]->days[4], P.course[i]->days[5]);
 
-
 		course[i]->currentRuang = P.course[i]->currentRuang;
 		course[i]->currentRuangIdx = P.course[i]->currentRuangIdx;
 		course[i]->currentStartTime = P.course[i]->currentStartTime;
 		course[i]->currentDay = P.course[i]->currentDay;
-
 	}
-
 }
 
 Problem::~Problem() {
@@ -302,14 +270,11 @@ void Problem::initByRandom() {
 			course[i]->currentRuangIdx = randomlyChosenRoomIdx;
 			course[i]->currentRuang = room[randomlyChosenRoomIdx]->ruangName;
 
-
-			// * Set hari dulu
+			// * Set hari
 			do {
 				randomlyChosenDay = rnd.nextInt(1, 5);
 			} while(!course[i]->isDayAvail(randomlyChosenDay));
 			course[i]->currentDay = randomlyChosenDay;
-			// Sayangnya : ini bisa kebobol kalo Ruangannya ga available
-			// Tapi itulah LOCAL SEARCH, sering dibiarin aja salah duluan, ntar akhir2nya jadi bener...
 
 			// * Set waktu
 			int courseBegin = course[i]->getStartTime(),
@@ -317,13 +282,8 @@ void Problem::initByRandom() {
 				courseDuration = course[i]->getDuration();
 			randomlyChosenStartTime = rnd.nextInt(courseBegin, courseEnd-courseDuration);
 			course[i]->currentStartTime = randomlyChosenStartTime;
-			// Tidak perlu di do..while karena random nya PASTI masuk di range courseBegin dan courseEnd
 		} while (!room[randomlyChosenRoomIdx]->isTimeAvail(course[i]->currentStartTime, course[i]->currentStartTime + course[i]->duration)
 				|| !room[randomlyChosenRoomIdx]->isDayAvail(randomlyChosenDay));
-		// do while sampe dapet Ruang yang available;
-
-
-		// ** Bisa juga alternatifnya, set hari dan waktu bersamaan
 	}
 }
 
@@ -363,7 +323,6 @@ void Problem::solveUsingHill(int maxRestart) {
 				}
 			}
 		}
-		cout << "---" << countRestart << " " << countConflictCourses() << " " << minConflict << "---" << endl;
 		countRestart++;
 		if (countConflictCourses() < minConflict) {
 			minConflict = countConflictCourses();
@@ -371,17 +330,10 @@ void Problem::solveUsingHill(int maxRestart) {
 		}
 	} while (tempEvalValue>0 && countRestart<=maxRestart);
 	*this = minimum;
-	/*for(int i=0; i<nCourses; i++) {
-		cout << *course[i] << endl;
-	}*/
 }
 
 void Problem::solveUsingSA(double temperature, double descentRate, int n, int maxSteps) {
 	initByRandom();
-	/*for(int i=0; i<nCourses; i++) {
-		cout << *course[i] << endl;
-	}*/
-
 	int stepCounter = 0;
 	Problem tempSolution = *this;
 	int tempEvalValue = countConflictCourses();
@@ -389,28 +341,19 @@ void Problem::solveUsingSA(double temperature, double descentRate, int n, int ma
 	while(tempEvalValue > 0 && stepCounter++ < maxSteps) {
 		for(int i = 0; i < n; i++) {
 			tempSolution = modifySolution(*this);
-/*			for(int i=0; i<nCourses; i++) {
-				cout << *tempSolution.course[i] << endl;
-			}*/
-
 			int newEvalValue = tempSolution.countConflictCourses();
 			int deltaEval = newEvalValue - tempEvalValue;
 			if(deltaEval < 0) {
 				*this = tempSolution;
 				tempEvalValue = countConflictCourses();
 			} else if (exp(-deltaEval/max(0.0,temperature)) > rnd.nextDouble()) {
-				//cout << "pangkat " << exp(-deltaEval/max(0.0,temperature)) << " " << -deltaEval << " " << temperature << endl;
 				*this = tempSolution;
 				tempEvalValue = countConflictCourses();
 			}
-			//cout << "step : " << (stepCounter-1)*n+i << " conflict : " << tempEvalValue << endl;
 			if(tempEvalValue == 0) break;
 		}
 		temperature -= descentRate;
 	}
-	/*for(int i=0; i<nCourses; i++) {
-		cout << *course[i] << endl;
-	}*/
 }
 
 void Problem :: solveUsingGA(int nSample, int nCycle) {
@@ -424,41 +367,6 @@ void Problem :: solveUsingGA(int nSample, int nCycle) {
         course[i] -> currentStartTime = G->getSolutionStartTime(i);
     }
     delete G;
-    /*for (int i = 0; i < nCourses; i++) {
-        cout << *course[i] << endl;
-    }*/
-}
-
-void Problem::solveUsingKocokan(int maxSteps) {
-	initByRandom();
-
-	int stepCounter = 0;
-	while(!isSolved() && stepCounter++ < maxSteps) {
-		// Pilih variabel yg conflict
-		int randomlySelectedVariable = rnd.nextInt(nCourses);
-		// ** gebleknya.. krn ga kepikiran isConflictingVariable nya gimana implementasinya
-
-		// Set ulang secara randomly
-		// ** Yg modif adalah : ruangannya, harinya
-		// ** Jam kuliah akan menyesuaikan dengan randomize
-		if(course[randomlySelectedVariable]->isButuhRuang()) { // PASTI HARINYA
-			int randomlyChosenDay;
-			do {
-				randomlyChosenDay = rnd.nextInt(1, 5);
-			} while(!course[randomlySelectedVariable]->isDayAvail(randomlyChosenDay));
-			course[randomlySelectedVariable]->currentDay = randomlyChosenDay;
-		}
-
-		// Measure performance
-		// *********************** Save it for later ***********************
-	}
-	// isSolved() OR stepCounter >= maxSteps
-	if(stepCounter < maxSteps) cout << "YAY" << endl;
-	else cout << "NAH" << endl;
-	/*
-	for(int i=0; i<nCourses; i++) {
-		cout << *course[i] << endl;
-	}*/
 }
 
 Problem Problem::modifySolution(Problem P) {
@@ -489,8 +397,6 @@ Problem Problem::modifySolution(Problem P) {
 			randomlyChosenDay = P.rnd.nextInt(1, 5);
 		} while(!P.course[randomlyChosenCourse]->isDayAvail(randomlyChosenDay));
 		P.course[randomlyChosenCourse]->currentDay = randomlyChosenDay;
-		// Sayangnya : ini bisa kebobol kalo Ruangannya ga available
-		// Tapi itulah LOCAL SEARCH, sering dibiarin aja salah duluan, ntar akhir2nya jadi bener...
 
 		// * Set waktu
 		int courseBegin = P.course[randomlyChosenCourse]->getStartTime(),
@@ -498,7 +404,6 @@ Problem Problem::modifySolution(Problem P) {
 			courseDuration = P.course[randomlyChosenCourse]->getDuration();
 		randomlyChosenStartTime = P.rnd.nextInt(courseBegin, courseEnd-courseDuration);
 		P.course[randomlyChosenCourse]->currentStartTime = randomlyChosenStartTime;
-		// Tidak perlu di do..while karena random nya PASTI masuk di range courseBegin dan courseEnd
 	} while (!P.room[randomlyChosenRoomIdx]->isTimeAvail(P.course[randomlyChosenCourse]->currentStartTime,
 			P.course[randomlyChosenCourse]->currentStartTime + P.course[randomlyChosenCourse]->duration)
 			|| !room[randomlyChosenRoomIdx]->isDayAvail(randomlyChosenDay));
@@ -511,7 +416,7 @@ Problem Problem::modifySolution(Problem P) {
 **********************************************/
 bool Problem::isSolved() {
 	int randomlyIsSolved = rnd.nextInt(1, 100);
-	if(randomlyIsSolved < 3) return 1; // 2% Solved , geblek bangettt
+	if(randomlyIsSolved < 3) return 1;
 	else return 0;
 }
 
@@ -525,7 +430,6 @@ int Problem::countConflictCourses() {
 					maks = max(course[i]->currentStartTime, course[j]->currentStartTime);
 					minim = min(course[i]->currentStartTime + course[i]->getDuration(),
 								course[j]->currentStartTime + course[j]->getDuration());
-					// tambah sesuai banyaknya jam yang bentrok
 					if(maks <= minim) {
 						count += minim - maks;
 					}
--- Src/RandomGenerator.hpp
+++ Src/RandomGenerator.hpp
@@ -11,7 +11,6 @@ using namespace std;
 
 class RandomGenerator
 {
-
     public:
     	RandomGenerator();
         static RandomGenerator* getInstance() { return _instance; }
